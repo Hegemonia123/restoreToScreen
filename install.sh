5b37dfe0b9801afa137b07cd2370886c25c589a6
He <hegeh123@outlook.com>
2023-04-09 15:55:18 +0300

    Improve install script

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 if [[ `kpackagetool5 -l | grep "restoreToScreen"` ]]; then
     echo "Updating..."
     kpackagetool5 --type=KWin/Script -u .
+    kwriteconfig5 --file kwinrc --group Plugins --key restoreToScreen false
+    qdbus org.kde.KWin /KWin reconfigure
+    sleep 2;
+    kwriteconfig5 --file kwinrc --group Plugins --key restoreToScreen true
+    qdbus org.kde.KWin /KWin reconfigure
 else
     echo "Installing..."
     kpackagetool5 --type=KWin/Script -i .
